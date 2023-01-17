d39f92e219f12d84c7365bf674f8f3e6e765b450
levi106 <16788901+levi106@users.noreply.github.com>
2023-01-17 15:08:27 +0900

    load certificate

--- src/SimpleApi/Program.cs
+++ src/SimpleApi/Program.cs
@@ -1,11 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var certPath = Environment.GetEnvironmentVariable("SIMPLEAPI_CERT_PATH");
+var certPassword = Environment.GetEnvironmentVariable("SIMPLEAPI_CERT_PASSWORD");
+
 builder.WebHost.ConfigureKestrel((context, options) =>
 {
     options.ListenAnyIP(5003, listenOptions =>
     {
         listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2AndHttp3;
-        listenOptions.UseHttps();
+        if (string.IsNullOrEmpty(certPath))
+        {
+            listenOptions.UseHttps();
+        }
+        else
+        {
+            listenOptions.UseHttps(certPath, certPassword);
+        }
     });
 });
 var app = builder.Build();

c1ef60651b10e7a62f9d0fc3f56d8a963ec5eab3
levi106 <16788901+levi106@users.noreply.github.com>
2023-01-18 02:55:47 +0900

    add sample client

---
+++ src/Http3Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Quic;
+
+using var client = new HttpClient()
+{
+    DefaultRequestVersion = HttpVersion.Version30,
+    // DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrLower
+    // DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher
+    DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact
+};
+
+string requestUrl = args.Length > 0 ? args[0] : "https://localhost:5003";
+
+if (!QuicConnection.IsSupported)
+{
+    Console.WriteLine($"QUIC is not supported.");
+}
+else
+{
+    Console.WriteLine($"QUIC is supported.");
+}
+Console.WriteLine($"Send request to {requestUrl}");
+HttpResponseMessage res = await client.GetAsync(requestUrl);
+string body = await res.Content.ReadAsStringAsync();
+
+Console.WriteLine($"status: {res.StatusCode}, version: {res.Version}, " +
+    $"body: {body.Substring(0, Math.Min(100, body.Length))}");
